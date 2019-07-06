5245bb253b7f84ecb0bfe85854e089d510a8e2d6
MorrayLeuz <kamaraelhadji5@gmail.com>
2019-07-06 19:37:10 +0000

    first symfony file modification

--- HelloController.php
+++ HelloController.php
@@ -3,6 +3,6 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;// Pour importer une classe
 class HelloController{
     public function home(){
-        return new Response("<html><body> Bienvenue en Symfony 4</body></html>");
+        return new Response("<html><body> Bienvenue en Symfony 4 tres interressant pour un apprenant de maitriser ce framework</body></html>");
     }
 }
